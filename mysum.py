61fb22aa6508e63604fd2db0a529f4bf53d57aba
BEN-HUR PEREIRA BORGES DOS SANTOS <138973267+botlorien@users.noreply.github.com>
2025-01-21 16:40:05 -0400

    Add files via upload

---
+++ mysum.py
@@ -0,0 +1,21 @@
+import functools
+import operator
+from collections.abc import Iterable
+from typing import overload, Union, TypeVar
+
+T = TypeVar('T')
+S = TypeVar('S')
+
+
+@overload
+def sum(it: Iterable[T]) -> Union[T, int]: ...
+@overload
+def sum(it: Iterable[T], /, start: S) -> Union[T, S]: ...
+
+
+def sum(it, /, start=0):
+    return functools.reduce(operator.add, it, start)
+
+
+if __name__ == '__main__':
+    print(sum((1, 2), 3))
